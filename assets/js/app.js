dc42b1396f326a2c88e1af07a4ca05763b64c4f9
MatPic <mateo.piccarreta@gmail.com>
2020-04-20 15:55:25 +0200

    Home terminé (sauf responsive)

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,18 @@
-import $ from 'jquery'
+import $ from 'jquery';
+import arrowScroll from './arrowScroll';
+
+$(document).ready(function () {
+    $(window).scroll(() => {
+        if ($(window).scrollTop() > 0) {
+            $('#main-nav').addClass('scrolled')
+            $('li').addClass('scrolled')
+            $('.btn').addClass('scrolled')
+        } else {
+            $('#main-nav').removeClass('scrolled')
+            $('li').removeClass('scrolled')
+            $('.btn').removeClass('scrolled')
+        }
+    })
+
+    arrowScroll()
+})
---
+++ assets/js/arrowScroll.js
@@ -0,0 +1,16 @@
+import $ from 'jquery'
+
+export default function arrowScroll () {
+  const arrow = $('#arrow-scroll')
+  arrow.click( function (e) {
+    e.preventDefault()
+    $('footer').get(0).scrollIntoView({ behavior: 'smooth' })
+  })
+  $(window).scroll(() => {
+    if ($(window).scrollTop() > 0) {
+        arrow.addClass('hidden')
+    } else {
+        arrow.removeClass('hidden')
+    }
+  })
+}
